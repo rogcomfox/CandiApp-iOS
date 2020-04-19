d38ca62a08fb28afabfe5819cdf235d49ed7d1c1
rogcomfox <joeg.11021996@gmail.com>
2020-04-19 16:38:43 +0700

    Initial Commit

---
+++ CandiAppIos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CandiAppIos
+//
+//  Created by Joe G on 19/04/20.
+//  Copyright © 2020 Nusantarian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
